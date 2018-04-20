ee0c183e61b1ad8f891300d130f4af38d2d49ab1
GAGAN.TYAGI <msharma598@gmail.com>
2018-04-20 12:21:28 +0530

    App changes test

--- src/main/java/com/etechon/multithreading/App.java
+++ src/main/java/com/etechon/multithreading/App.java
@@ -1,13 +1,13 @@
 package com.etechon.multithreading;
 
 /**
- * Hello world!
+ * Multithreading Samples.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
     }
 }
